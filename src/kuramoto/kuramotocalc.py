aacf82957120dff060cdd0af140d9e95bb3d8c86
Omerbass <omer.basri@gmail.com>
2026-07-22 15:17:49 +0300

    Removed Kuramoto module

--- src/kuramoto/kuramotocalc.py
+++
@@ -1,8 +0,0 @@
-import scipy as sp
-import numpy as np
-
-def freqdist_lorenzian(omega, gamma=1.0):
-    return gamma / (np.pi * (omega**2 + gamma**2))
-
-def kuramoto_critical_coupling(gamma=1.0, D=1.0):
-    return 2.0 / (gamma + D)
